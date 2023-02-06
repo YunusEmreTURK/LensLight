aedbd8fa5e9c18f5ef58cae5543da8102fc05a30
Yunus Emre <72800024+YunusEmreTURK@users.noreply.github.com>
2023-02-06 12:56:28 +0300

    thunder_client

--- controllers/photoController.js
+++ controllers/photoController.js
@@ -1,11 +1,37 @@
 import Photo from "../models/photoModel.js"; // import işlemi yaptığımız için .js i ekliyoruz
 
-const createPhoto = (req, res) => {
-    const photo = Photo.create(req.body);
-    res.status(201).json({
-        succeded: true,
-        photo,
+const createPhoto = async (req, res) => {
+
+    try {
+
+        const photo = await Photo.create(req.body);
+        res.status(201).json({
+            succeded: true,
+            photo,
     });
+        
+    } catch (error) {
+        res.status(500).json({
+            succeded: false,
+            error
+        });   
+    }  
 };
 
-export {createPhoto};
+const getAllPhotos = async (req, res) => {
+    try {
+        const photos = await Photo.find({})// boş obje bütün fotoğrafları bulmayı sağlamak için
+        res.status(200).json({
+            succeded: true,
+            photos
+        })
+    } catch (error) {
+        res.status(500).json({
+            succeded: false,
+            error
+        }); 
+        
+    }
+}
+
+export {createPhoto, getAllPhotos};
--- routes/photoRoute.js
+++ routes/photoRoute.js
@@ -3,6 +3,9 @@ import * as photoController from '../controllers/photoController.js';
 
 const router = exrpess.Router();
 
-router.route('/').post(photoController.createPhoto); // buradaki "/"" "/photos" un devamı olan yani /photos/. "/" yerine "/test olsaydı /photos/test yönlendirmesi anlamına gelirdi"
+router
+    .route('/')
+    .post(photoController.createPhoto)
+    .get(photoController.getAllPhotos); // buradaki "/"" "/photos" un devamı olan yani /photos/. "/" yerine "/test olsaydı /photos/test yönlendirmesi anlamına gelirdi"
 
 export default router;
