7054b6e043e3ff0c0b3f547e18a291b1d6970da2
Yunus Emre <72800024+YunusEmreTURK@users.noreply.github.com>
2023-02-25 21:56:49 +0300

    render_func  active_link

--- controllers/photoController.js
+++ controllers/photoController.js
@@ -21,10 +21,11 @@ const createPhoto = async (req, res) => {
 const getAllPhotos = async (req, res) => {
     try {
         const photos = await Photo.find({})// boş obje bütün fotoğrafları bulmayı sağlamak için
-        res.status(200).json({
-            succeded: true,
-            photos
+        res.status(200).render("photos",{
+            photos,
+            link: `photos`,
         })
+
     } catch (error) {
         res.status(500).json({
             succeded: false,
